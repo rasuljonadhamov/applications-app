cf1c25476fc1581a99f96fde9e352055b30f2578
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-24 23:47:54 +0500

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Application from "./components/Application";
 import svg from "/src/images/noApplication.svg";
 
